c0a2369b79094111278d475d61608ef5e03be6e7
David Berka <davidberka2005@gmail.com>
2021-08-24 13:48:16 +0200

    zapasy-done2

--- app/js/main.js
+++ app/js/main.js
@@ -62,84 +62,125 @@ $(function () {
    }
  });
 
- // vstupenky btn
-const btns = document.querySelectorAll('.zapasy-page__button');
+//  vstupenky btn
+// const btns = document.querySelectorAll('.zapasy-page__button');
 
-btns.forEach(btn => {
-  const type = btn.parentNode.parentNode.className.split(' ')[1];
-  if (type === 'away') {
-    btn.style.display = 'none';
+// btns.forEach(btn => {
+//   const type = btn.parentNode.parentNode.className.split(' ')[1];
+//   if (type === 'away') {
+//     btn.style.display = 'none';
+//   }
+// })
+
+//  //filter buttons - global
+// let typeOfMatch = 'league';
+// let filterClass = '';
+
+// $('.zapasy_container').hide();
+// $('.zapasy_container.' + typeOfMatch).show();
+
+//  // filter buttons - league, pre-season
+// $(".liga-row a").each(function() {
+//     $(this).on("click", function(){
+//       btns.forEach(btn => {
+//         const type = btn.parentNode.parentNode.className.split(' ')[1];
+//         if (type === 'away') {
+//           btn.style.display = 'none';
+//         }
+//       })
+
+//       typeOfMatch = this.className.split(' ')[0];
+//       filterClass = '';
+//       $('#filter a.active').removeClass('active');
+//       $('#filter a.all').addClass('active');
+//       $('.zapasy_container.' + typeOfMatch + ' .zapasy-page__row').show();
+
+//       $('.liga-row a').removeClass('active');
+//       $(this).addClass('active');
+    
+//       $('.zapasy_container').hide();
+
+//       if (typeOfMatch === 'league') {
+//         $('.zapasy_container.' + typeOfMatch).show();
+//       } else if (typeOfMatch === 'pre-season') {
+//         $('.zapasy_container.' + typeOfMatch).show();
+//       } else {
+//         $('.zapasy_container').show();
+//       }
+//     });
+// });
+
+
+// // filter buttons - home, away, all
+// $('.zapasy_container.' + typeOfMatch + ' .zapasy-page__row').show();
+
+// $("#filter a").each(function() {
+//     $(this).on("click", function(){
+//       filterClass = this.className.split(' ')[0];
+
+//       btns.forEach(btn => {
+//         const type = btn.parentNode.parentNode.className.split(' ')[1];
+//         if (type === 'away') {
+//           btn.style.display = 'none';
+//         }
+//       })
+
+//       $('#filter a').removeClass('active');
+//       $(this).addClass('active');
+    
+//       $('.' + typeOfMatch + ' .zapasy-page__row').hide();
+      
+//       if (filterClass === 'home') {
+//         $('.' + typeOfMatch + ' .zapasy-page__row.' + filterClass).show();
+//       } else if (filterClass === 'away') {
+//         $('.' + typeOfMatch + ' .zapasy-page__row.' + filterClass).show();
+//       } else {
+//         $('.' + typeOfMatch + ' .zapasy-page__row').show();
+//       }
+//     });
+// });
+
+
+// filter btns - home, away, all
+const targetFilter = localStorage.getItem('target-filter');
+
+window.addEventListener('load', () => {
+  const linkClass = targetFilter && document.querySelector(`a[data-target="${targetFilter}"]`).className.split(' ')[0];
+  if (linkClass) {
+    $('#filter a.active').removeClass('active');
+    $('#filter a.' + linkClass).addClass('active');
+  } else {
+    $('#filter a.all').addClass('active');
   }
+  
 })
 
- //filter buttons - global
-let typeOfMatch = 'league';
-let filterClass = '';
+$('#filter a').each(function () {
+  $(this).on("click", function () {
+      localStorage.setItem('target-filter', $(this).data("target"));
+  })
+})
 
-$('.zapasy_container').hide();
-$('.zapasy_container.' + typeOfMatch).show();
+// match btns - league, pre-season
+const targetCategory = localStorage.getItem('target-category');
 
- // filter buttons - league, pre-season
-$(".liga-row a").each(function() {
-    $(this).on("click", function(){
-      btns.forEach(btn => {
-        const type = btn.parentNode.parentNode.className.split(' ')[1];
-        if (type === 'away') {
-          btn.style.display = 'none';
-        }
-      })
+window.addEventListener('load', () => {
+  const linkClass = targetCategory && document.querySelector(`a[data-target="${targetCategory}"]`).className.split(' ')[0];
+  if (linkClass) {
+    $('.liga-row a.active').removeClass('active');
+    $('.liga-row a.' + linkClass).addClass('active');
+  } else {
+    $('.liga-row a.league').addClass('active');
+  }
+  
+})
 
-      typeOfMatch = this.className.split(' ')[0];
-      filterClass = '';
-      $('#filter a.active').removeClass('active');
-      $('#filter a.all').addClass('active');
-      $('.zapasy_container.' + typeOfMatch + ' .zapasy-page__row').show();
-
-      $('.liga-row a').removeClass('active');
-      $(this).addClass('active');
-    
-      $('.zapasy_container').hide();
-
-      if (typeOfMatch === 'league') {
-        $('.zapasy_container.' + typeOfMatch).show();
-      } else if (typeOfMatch === 'pre-season') {
-        $('.zapasy_container.' + typeOfMatch).show();
-      } else {
-        $('.zapasy_container').show();
-      }
-    });
-});
-
-
-// filter buttons - home, away, all
-$('.zapasy_container.' + typeOfMatch + ' .zapasy-page__row').show();
-
-$("#filter a").each(function() {
-    $(this).on("click", function(){
-      filterClass = this.className.split(' ')[0];
-
-      btns.forEach(btn => {
-        const type = btn.parentNode.parentNode.className.split(' ')[1];
-        if (type === 'away') {
-          btn.style.display = 'none';
-        }
-      })
-
-      $('#filter a').removeClass('active');
-      $(this).addClass('active');
-    
-      $('.' + typeOfMatch + ' .zapasy-page__row').hide();
-      
-      if (filterClass === 'home') {
-        $('.' + typeOfMatch + ' .zapasy-page__row.' + filterClass).show();
-      } else if (filterClass === 'away') {
-        $('.' + typeOfMatch + ' .zapasy-page__row.' + filterClass).show();
-      } else {
-        $('.' + typeOfMatch + ' .zapasy-page__row').show();
-      }
-    });
-});
-
+$('.liga-row a').each(function () {
+  $(this).on("click", function () {
+    localStorage.setItem('target-category', $(this).data("target"));
+    localStorage.setItem('target-filter', 'all');
+  })
+})
 
 /// score
 var n=$("#scoreboard_carousel").data("initialslider");null==n&&(n=1),$("#scoreboard_carousel").slick({infinite:!1,arrows:!0,dots:!1,autoplay:!1,slidesToShow:5,slidesToScroll:1,centerMode:true,swipeToSlide:!0,initialSlide:n-1,responsive:[{breakpoint:1650,settings:{slidesToShow:4,slidesToScroll:1}},{breakpoint:1440,settings:{slidesToShow:3,slidesToScroll:1}},{breakpoint:1100,settings:{slidesToShow:2,slidesToScroll:1}},{breakpoint:750,settings:{slidesToShow:1,slidesToScroll:1,dots:!1}},{breakpoint:440,settings:{slidesToShow:1,slidesToScroll:1,dots:!1,arrows:!0,centerMode:!1}}]});
